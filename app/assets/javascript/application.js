9ee7be50e91d10d0bfcb69d7291f649331b55e7b
Mārtiņš Poļakovs <martins.polakovs@tietoevry.com>
2022-10-31 00:35:46 +0200

    Add jQuery UI

--- app/assets/javascript/application.js
+++ app/assets/javascript/application.js
@@ -1,2 +1,3 @@
 //= require jquery-3.6.1
+//= require jquery-ui
 //= require src/main
--- app/assets/javascript/src/main.js
+++ app/assets/javascript/src/main.js
@@ -7,4 +7,29 @@ window.addEventListener("DOMContentLoaded", function() {
 
 $(function() {
   $("#sprockets-jquery-hello").text("Hello from JavaScript (Sprockets + jQuery)!")
+
+  $("#jquery-ui-datepicker").datepicker()
+
+  $("#jquery-ui-datepicker-trigger").button({
+    icon: "ui-icon-calendar"
+  }).on("click", function() {
+    $("#jquery-ui-datepicker").datepicker("show")
+  })
+
+  $("#open-dialog").on("click", function() {
+    const dateValue = $("#jquery-ui-datepicker").datepicker("getDate")
+    let formattedDate = 'nothing :('
+    if (dateValue) {
+      formattedDate = $.datepicker.formatDate('dd.mm.yy', dateValue)
+    }
+
+    $("#dialog [data-field='selected-date']").text(formattedDate)
+    $("#dialog").dialog({
+      buttons: {
+        "Close": function() {
+          $(this).dialog( "close" );
+        },
+      }
+    })
+  })
 })
